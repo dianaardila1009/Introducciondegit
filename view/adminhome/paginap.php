492ff93a17a96f4d4a4634e94a08ad74967cad2c
dianaardila1009 <dianapaolaardilaalfonso@gmail.com>
2026-06-25 14:34:27 -0500

    Modificación para Página Principal

--- view/adminhome/paginap.php
+++ view/adminhome/paginap.php
@@ -4,13 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trabajemos juntos</title>
-    <link rel="stylesheet" href="../adminhome/assets/css/estilos.css">
+    <link rel="stylesheet" href="../../assets/css/estilos.css">
 </head>
 <body>
     <h1>Trabajemos juntos con GitHub</h1>
     <h2>Bienvenid@s a Nuestra Página</h2>
-    <img src="assets/images/inicio.png" alt="">
-    
+    <img class="imagen-inicio" src="../../assets/images/inicio.png" alt="">
+    <br>
+    <div class="contenido">
+        <h3>Bienvenido a nuestra página de trabajo colaborativo en GitHub.</h3>
+        <h4>En este espacio, nos enfocamos en la colaboración y el desarrollo de proyectos de manera conjunta. Aquí encontrarás recursos, documentación y herramientas 
+            para facilitar el trabajo en equipo.</h4>
+        <h4>Explora nuestras secciones, participa en discusiones y contribuye a nuestros
+        proyectos. Juntos, podemos lograr grandes cosas y aprender unos de otros.</h4>
+        <h4>¡Gracias por unirte a nuestra comunidad y esperamos que disfrutes tu experiencia colaborativa!</h4>
+        <br>
+        <h4>Recuerda que la colaboración es clave para el éxito de cualquier proyecto. No dudes en compartir tus ideas, hacer preguntas y ofrecer tu ayuda a otros miembros de la comunidad.</h4>
+        <h4>¡Estamos emocionados de tenerte con nosotros y esperamos que tu participación sea
+        enriquecedora y gratificante!</h4>
+    </div>
     
 </body>
 </html>
